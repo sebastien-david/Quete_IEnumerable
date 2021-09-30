6c5cc96ca34a3eb8d81ce4a2a752022b47772534
sebastien-david <sebastien.david08@gmail.com>
2021-09-30 21:28:27 +0200

    Ajoutez des fichiers projet.

---
+++ Quete_IEnumerable/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quete_IEnumerable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] tableauEntier = new int [] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            
+            Console.WriteLine(" Voici un tableau d'entier: \n");
+
+            foreach(int entier in tableauEntier)
+            {
+                Console.WriteLine("   " + entier);
+            }
+            
+            Console.Write("\n Choisir la limite maximale des valeurs à garder: ");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("\n--------------------------------------------------\n");
+
+            int[] valeurMaxTableauEntier = tableauEntier.Where((x) => x <= n).ToArray();
+            double average = (double)valeurMaxTableauEntier.Average();
+
+            Console.WriteLine(" Le nouveau tableau d'entier est: \n");
+
+            foreach (int entier in valeurMaxTableauEntier)
+            {
+                Console.WriteLine("   " + entier);
+            }
+
+            Console.Write("\n La moyenne de ce nouveau tableau = " + average);
+
+            Console.ReadLine();
+        }
+    }
+}
